71ba1388e8c7fce1ef751374bed9ecc9051c3ab3
Shikhar Gupta <sgupt330@asu.edu>
2023-10-07 02:39:57 -0700

    Added autoscaling feature

--- autoscaler/autoscale.py
+++ autoscaler/autoscale.py
@@ -2,82 +2,166 @@ import time
 import boto3
 import os
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION = 'us-east-1'  
-
-queue_url = 'https://sqs.us-east-1.amazonaws.com/../RequestQueue.fifo'
+from dotenv import dotenv_values
 
 MIN_INSTANCES = 1
 MAX_INSTANCES = 20
 
-ec2 = boto3.client('ec2', region_name=region_name,
-                   aws_access_key_id=aws_access_key_id,
-                   aws_secret_access_key=aws_secret_access_key)
-sqs = boto3.client('sqs', region_name=region_name,
-                   aws_access_key_id=aws_access_key_id,
-                   aws_secret_access_key=aws_secret_access_key)
 
-image_id = 'ami-09c6ef0459a2ff40e'
-instance_type = 't2.micro'
-key_name = 'cc_project1'
+class AutoScale:
+    def __init__(self, config):
+        self.image_ami_id = 'ami-09c6ef0459a2ff40e'
+        self.instance_type = 't2.micro'
+        self.subnet_id = "subnet-09d6065e5b1bce424"
+        self.key_name = 'cc_project1'
+        self.security_group = "sg-0afd4e0aeafb5b473"
+        self.instance_map = {}
+        self.request_queue_url = config.get('REQUEST_QUEUE_URL')
+        self.ec2_client = boto3.client('ec2', region_name=config.get('AWS_DEFAULT_REGION'),
+                                       aws_access_key_id=config.get('AWS_ACCESS_KEY_ID'),
+                                       aws_secret_access_key=config.get('AWS_SECRET_ACCESS_KEY'))
 
-def create_instance():
-    try:
-        instances = ec2.run_instances(
-            ImageId=image_id,
-            MinCount=1,
-            MaxCount=1,
-            InstanceType=instance_type,
-            KeyName=key_name
-        )
-        instance_id = instances['Instances'][0]['InstanceId']
-        return instance_id
-    except Exception as e:
-        print(f"Failed to create instance: {e}")
-        return None
-        
-def get_approx_total_msgs():
-    try:
-        response = sqs.get_queue_attributes(
-            QueueUrl=queue_url,
-            AttributeNames=['ApproximateNumberOfMessages']
-        )
-        total_msgs = int(response['Attributes']['ApproximateNumberOfMessages'])
-        return total_msgs
-    except Exception as e:
-        print(f"Failed to get queue attributes: {e}")
-        return None
-        
-def get_num_of_instances():
-    try:
-        instances = ec2.describe_instance_status(IncludeAllInstances=True)
-        running_instances = [i for i in instances['InstanceStatuses'] if
-                             i['InstanceState']['Name'] in ('pending', 'running')]
-        total_running_instances = len(running_instances)
-        return total_running_instances
-    except Exception as e:
-        print(f"Failed to get instance status: {e}")
-        return None
+        self.sqs_client = boto3.client('sqs', region_name=config.get('AWS_DEFAULT_REGION'),
+                                       aws_access_key_id=config.get('AWS_ACCESS_KEY_ID'),
+                                       aws_secret_access_key=config.get('AWS_SECRET_ACCESS_KEY'))
+        self.user_script = """
+        cd /home/ubuntu/Project-IaaS/apptier; python3 apptier.py &&
+        """
+
+    def create_instance(self, iid):
+        try:
+            block_device = [
+                {
+                    'DeviceName': "ebs%s" % iid,
+                    'Ebs': {
+                        'DeleteOnTermination': True,
+                        'VolumeSize': 20,
+                        'VolumeType': 'gp2'
+                    }
+                },
+            ]
+
+            response = self.ec2_client.run_instances(ImageId=self.image_ami_id,
+                                                     InstanceType=self.instance_type,
+                                                     SubnetId=self.subnet_id,
+                                                     SecurityGroupIds=[self.security_group],
+                                                     UserData=self.user_script,
+                                                     MinCount=1, MaxCount=1,
+                                                     BlockDeviceMappings=block_device,
+                                                     KeyName=self.key_name,
+                                                     TagSpecifications=[
+                                                         {
+                                                             'ResourceType': 'instance',
+                                                             'Tags': [
+                                                                 {
+                                                                     'Key': 'Name',
+                                                                     'Value': "app-instance%s" % iid
+                                                                 }
+                                                             ]
+                                                         }
+                                                     ])
+
+            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                instance_id = response['Instances'][0]['InstanceId']
+                self.ec2_client.get_waiter('instance_running').wait(
+                    InstanceIds=[instance_id]
+                )
+                print('Success! instance:', instance_id, 'is created and running')
+                self.instance_map[iid] = instance_id
+                return True
+            return False
+        except Exception as e:
+            print(f"Failed to create instance: {e}")
+            return False
+
+    def remove_instance(self, iid):
+        try:
+            response = self.ec2_client.terminate_instances(
+                InstanceIds=[
+                    self.instance_map[iid],
+                ],
+                DryRun=True
+            )
+            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                instance_id = response['Instances'][0]['InstanceId']
+                self.ec2_client.get_waiter('instance_stopped').wait(
+                    InstanceIds=[instance_id]
+                )
+                print('Success! instance:', instance_id, 'is stopped')
+                return True
+            return False
+        except Exception as e:
+            print(f"Failed to remove instance: {e}")
+            return False
+
+    def scaleup(self, current, required):
+        if required >= MAX_INSTANCES:
+            return current
+
+        while current <= required:
+            self.create_instance(current)
+            current += 1
+            time.sleep(60)
+        return current
+
+    def scaledown(self, current, required):
+        if required <= MIN_INSTANCES:
+            return current
+
+        while current >= required:
+            self.remove_instance(current)
+            current -= 1
+            time.sleep(60)
+        return current
+
+    def get_total_msgs(self):
+        try:
+            response = self.sqs_client.get_queue_attributes(
+                QueueUrl=self.request_queue_url,
+                AttributeNames=['ApproximateNumberOfMessages']
+            )
+            total_msgs = int(response['Attributes']['ApproximateNumberOfMessages'])
+            return total_msgs
+        except Exception as e:
+            print(f"Failed to get queue attributes: {e}")
+            return 1
+
+    # def get_num_of_instances(self):
+    #     try:
+    #         response = self.ec2_client.describe_instances(
+    #             Filters=[
+    #                 {
+    #                     "ImageId":  self.image_ami_id
+    #                 },
+    #             ],
+    #             DryRun=True,
+    #         )
+    #         running_instances = [i for i in instances['InstanceStatuses'] if
+    #                              i['InstanceState']['Name'] in ('pending', 'running')]
+    #         total_running_instances = len(running_instances)
+    #         return total_running_instances
+    #     except Exception as e:
+    #         print(f"Failed to get instance status: {e}")
+    #         return None
+
 
 def main():
+    config = dotenv_values()
+    auto_scale_obj = AutoScale(config)
+    current_instance_count = 1
     while True:
-        total_msgs = get_approx_total_msgs()
-        total_running_instances = get_num_of_instances()
-        total_app_instances = total_running_instances - 1 
+        total_msgs = auto_scale_obj.get_total_msgs()
+        if total_msgs > current_instance_count:
+            print(f"Messages in Input Queue: {total_msgs}. Scaling Up!")
+            current_instance_count = auto_scale_obj.scaleup(current_instance_count, total_msgs)
+        elif total_msgs < current_instance_count:
+            print(f"Messages in Input Queue: {total_msgs}. Scaling Down!")
+            current_instance_count = auto_scale_obj.scaledown(current_instance_count, total_msgs)
+        else:
+            print("Load is OK")
 
-        print(f"Messages in Input Queue: {total_msgs}")
-        print(f"Total App Instances: {total_app_instances}")
+        time.sleep(5)
 
-        if total_msgs > 0 and total_msgs > total_app_instances:
-            instances_to_launch = min(19 - total_app_instances, total_msgs - total_app_instances)
-            if instances_to_launch > 0:
-                for i in range(instances_to_launch):
-                    instance_id = create_instance()
-                    if instance_id:
-                        print(f"Launched instance {instance_id}")
-
-        time.sleep(3000) 
 
 if __name__ == "__main__":
     main()
